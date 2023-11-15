cbde6a2f83524a83042a576e448cffff957250a3
Johan Fredrik Wilvang <johan@wilvang.com>
2023-11-15 13:09:37 +0100

    Revert implementation of builder design pattern. Modified methods to require 4 parameters to create an object.

--- src/main/java/edu/ntnu/stud/transport/train/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/transport/train/TrainDeparture.java
@@ -21,16 +21,15 @@ import java.time.LocalTime;
  */
 
 public class TrainDeparture {
-
   // required fields
   private LocalTime departureTime;
   private int trainNumber;
-
-  // optional fields
-  private LocalTime delay;
   private String trainLine;
   private String destination;
+  // optional fields
+  private LocalTime delay;
   private int trackNumber;
+
 
 
   /**
@@ -44,16 +43,16 @@ public class TrainDeparture {
    * TrainDispatch class is responsible for setting a unique train number to the
    * train departure.
    *
-   * @param builder the specified builder.
    * @since 1.3.0
    */
-  public TrainDeparture(TrainBuilder builder) {
-    this.departureTime = builder.departureTime;
-    this.trainNumber = builder.trainNumber;
-    this.delay = builder.delay;
-    this.trainLine = builder.trainLine;
-    this.destination = builder.destination;
-    this.trackNumber = builder.trackNumber;
+  public TrainDeparture(String departureTime, int trainNumber, String trainLine, String destination) {
+    this.setDepartureTime(departureTime);
+    this.setTrainNumber(trainNumber);
+    this.setTrainLine(trainLine);
+    this.setDestination(destination);
+    this.trackNumber = -1;
+    this.delay = LocalTime.of(0, 0);
+
   }
 
   /**
@@ -116,137 +115,115 @@ public class TrainDeparture {
     return this.trackNumber;
   }
 
-  public static class TrainBuilder {
-    // required fields
-    private LocalTime departureTime;
-    private int trainNumber;
 
-    // optional fields
-    private LocalTime delay;
-    private String trainLine;
-    private String destination;
-    private int trackNumber;
+  /**
+   * Increment the train's delay with a specified number of minutes delayed. If the specified
+   * number of minutes delayed is less than zero, do nothing. The train departure time is
+   * incremented proportionally to the delayed time.
+   *
+   * @param minutesDelay the specified number of minutes delayed.
+   * @since 1.0.0
+   */
+  public void setDelay(int minutesDelay) {
+    if (minutesDelay > 0) {
+      this.delay = this.delay.plusMinutes(minutesDelay);
+      this.departureTime = this.departureTime.plusMinutes(minutesDelay);
+    }
+  }
+  /**
+   * Increment the train's delay with a specified number hours delayed and minutes delayed. The
+   * specified number of hours delayed or minutes delayed is only incremented with positive values.
+   * The train departure time is incremented proportionally to the delayed time.
+   *
+   * @param hoursDelay   the specified number of hours delayed.
+   * @param minutesDelay the specified number of minutes delayed.
+   * @since 1.0.0
+   */
+  public void setDelay(int hoursDelay, int minutesDelay) {
+    if (hoursDelay > 0) {
+      this.delay = this.delay.plusHours(hoursDelay);
+      this.departureTime = this.departureTime.plusHours(hoursDelay);
+    }
+    if (minutesDelay > 0) {
+      this.delay = this.delay.plusMinutes(minutesDelay);
+      this.departureTime = this.departureTime.plusMinutes(minutesDelay);
+    }
+  }
+  /**
+   * Set the train's departure time to the specified time of the departure. If the specified time
+   * is an invalid clock time, the departure time is set to 00:00.
+   *
+   * @param departureTime The specified time of the departure.
+   * @since 1.6.0
+  */
+  public void setDepartureTime(String departureTime) {
+    if (departureTime.matches("\\d{2}:\\d{2}")
+        && departureTime.length() == 5
+        && Integer.parseInt(departureTime.substring(0, 2)) < 24
+        && Integer.parseInt(departureTime.substring(3, 5)) < 60) {
+      this.departureTime = LocalTime.parse(departureTime);
+    } else {
+      this.departureTime = LocalTime.of(0, 0);
+    }
+  }
 
-    public TrainBuilder(int departureHours, int departureMinutes, int trainNumber){
-      this.departureTime = LocalTime.of(departureHours,departureMinutes);
-      this.delay = LocalTime.of(0,0);
+  /**
+   * Set train line to the new specified train line. If the specified train line is equal to null,
+   * the train line is set to "INVALID".
+   *
+   * @param trainLine the new specified train line.
+   * @since 1.2.0
+   */
+  public void setTrainLine(String trainLine) {
+    if (trainLine != null) {
+      this.trainLine = trainLine.toUpperCase();
+    } else {
+      this.trainLine = "INVALID";
+    }
+  }
+  /**
+   * Set the train's destination to a new specified destination. If the specified destination is
+   * equal to null, the destination is set to "INVALID".
+   *
+   * @param destination the new specified destination.
+   * @since 1.3.0
+   */
+  public void setDestination(String destination) {
+    if (destination != null) {
+      this.destination = destination;
+    } else {
+      this.destination = "INVALID";
+    }
+  }
+
+  /**
+   * Set the train number to a new specified train number. The train number's area of validity is
+   * a positive number up to 3 digits. If the specified train number is outside the area of
+   * validity, the train number is set to -1.
+   *
+   * @param trainNumber the new specified train number.
+   */
+  public void setTrainNumber(int trainNumber){
+    if (trainNumber > 0 && trainNumber < 1000){
       this.trainNumber = trainNumber;
-      this.destination = "";
-      this.trainLine = "";
-      this.trackNumber = -1;
+    } else {
+      this.trainNumber = -1;
     }
+  }
 
-    /**
-     * Increment the train's delay with a specified number of minutes delayed. If the specified
-     * number of minutes delayed is less than zero, do nothing. The train departure time is
-     * incremented proportionally to the delayed time.
-     *
-     * @param minutesDelay the specified number of minutes delayed.
-     * @since 1.0.0
-     */
-    public TrainBuilder setDelay(int minutesDelay) {
-      if (minutesDelay > 0) {
-        this.delay = this.delay.plusMinutes(minutesDelay);
-        this.departureTime = this.departureTime.plusMinutes(minutesDelay);
-      }
-      return this;
-    }
-
-    /**
-     * Increment the train's delay with a specified number hours delayed and minutes delayed. The
-     * specified number of hours delayed or minutes delayed is only incremented with positive
-     * values. The train departure time is incremented proportionally to the delayed time.
-     *
-     * @param hoursDelay   the specified number of hours delayed.
-     * @param minutesDelay the specified number of minutes delayed.
-     * @since 1.0.0
-     */
-    public TrainBuilder setDelay(int hoursDelay, int minutesDelay) {
-      if (hoursDelay > 0) {
-        this.delay = this.delay.plusHours(hoursDelay);
-        this.departureTime = this.departureTime.plusHours(hoursDelay);
-      }
-      if (minutesDelay > 0) {
-        this.delay = this.delay.plusMinutes(minutesDelay);
-        this.departureTime = this.departureTime.plusMinutes(minutesDelay);
-      }
-      return this;
-    }
-
-    /**
-     * Set the train's departure time to the specified time of day. If the specified time is an
-     * invalid clock time, the departure time is set to 00:00.
-     *
-     * @param departureHours   the specified hour of the day.
-     * @param departureMinutes the specified minute of the hour.
-     * @since 1.1.0
-     */
-    public TrainBuilder setDepartureTime(int departureHours, int departureMinutes) {
-      if (departureHours >= 0 && departureHours < 24
-          && departureMinutes >= 0 && departureMinutes < 60) {
-        this.departureTime = LocalTime.of(departureHours, departureMinutes);
-      } else {
-        this.departureTime = LocalTime.of(0, 0);
-      }
-      return this;
-    }
-
-    /**
-     * Set train line to the new specified train line. The train line is represented as a String.
-     * The string is made up of a prefix letter and a route number of one or two positive digits
-     * (1-99). The train type must be one of the following; 'L' (local train), 'F' (long-distance
-     * train), 'R' (regional train), "RE" (regional express train), "FLY" (flight train). If the
-     * prefix letter or the route number is outside the area of validity, the train line is set to
-     * "INVALID".
-     *
-     * @param trainLine the new specified train line.
-     * @since 1.2.0
-     */
-    public TrainBuilder setTrainLine(String trainLine) {
-      if (trainLine != null) {
-        this.trainLine = trainLine.toUpperCase();
-      } else {
-        this.trainLine = "INVALID";
-      }
-      return this;
-    }
-
-    /**
-     * Set the train's destination to a new specified destination. If the specified destination is
-     * equal to null, the destination is set to "INVALID".
-     *
-     * @param destination the new specified destination.
-     * @since 1.3.0
-     */
-    public TrainBuilder setDestination(String destination) {
-      if (destination != null) {
-        this.destination = destination;
-      } else {
-        this.destination = "INVALID";
-      }
-      return this;
-    }
-
-    /**
-     * Set the track number to a new specified track number. The track number's area of validity is
-     * a positive number up to the total amount of tracks in the station. If the specified track
-     * number is outside the area of validity, the track number is set to zero.
-     *
-     * @param trackNumber the new specified track number.
-     * @param totalTracks the total amount of tracks in the station.
-     * @since 1.3.0
-     */
-    public TrainBuilder setTrackNumber(int trackNumber, int totalTracks) {
-      if (trackNumber > 0 && trackNumber <= totalTracks) {
-        this.trackNumber = trackNumber;
-      } else {
-        this.trackNumber = 0;
-      }
-      return this;
-    }
-
-    public TrainDeparture build(){
-      return new TrainDeparture(this);
+  /**
+   * Set the track number to a new specified track number. The track number's area of validity is
+   * a positive number up to 100. If the specified track number is outside the area of validity, the
+   * track number is set to zero.
+   *
+   * @param trackNumber the new specified track number.
+   * @since 1.6.0
+   */
+  public void setTrackNumber(int trackNumber) {
+    if (trackNumber > 0 && trackNumber <= 100) {
+      this.trackNumber = trackNumber;
+    } else {
+      this.trackNumber = 0;
     }
   }
 }
