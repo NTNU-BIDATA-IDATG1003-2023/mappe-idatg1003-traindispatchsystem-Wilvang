86debedca40b9aeacff2ee42c5f027a68fb37a4e
Johan Fredrik Wilvang <johan@wilvang.com>
2023-11-17 10:12:35 +0100

    test: new unit tests for class TrainDeparture and TrainStation.
    Verson: 2.2.0

---
+++ src/test/java/edu/ntnu/stud/transport/train/TrainDepartureTest.java
@@ -0,0 +1,27 @@
+package edu.ntnu.stud.transport.train;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TrainDepartureTest {
+private TrainDeparture trainDepartureTest;
+  @BeforeEach
+  void setUp() {
+    trainDepartureTest = new TrainDeparture("12:00", 13,
+        "L2", "Oslo");
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void setDelayPositiveTest() {
+    trainDepartureTest.setDelay(120);
+    assertEquals(120, trainDepartureTest.getMinutesDelay(),
+        "The departure time was not changed");
+  }
+}
---
+++ src/test/java/edu/ntnu/stud/transport/train/TrainStationTest.java
@@ -0,0 +1,41 @@
+package edu.ntnu.stud.transport.train;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TrainStationTest {
+private TrainStation trainStationTest;
+  @BeforeEach
+  void setUp() {
+    trainStationTest = new TrainStation();
+    trainStationTest.addTrainDeparture("12:00", 13,
+        "L2", "Oslo");
+    trainStationTest.addTrainDeparture("12:30", 424,
+        "F3", "Trondheim");
+    trainStationTest.addTrainDeparture("13:00", 65,
+        "RE11", "Bergen");
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void searchByTrainNumberPositiveTest() {
+    assertEquals("Oslo", trainStationTest.searchByTrainNumber(13).next().getDestination(),
+        "The destination of the train with train number 13 was wrong");
+    assertFalse(trainStationTest.searchByTrainNumber(122).hasNext(),
+        "The search by train number method returned with a non existing train departure");
+  }
+
+  @Test
+  void setNewTrainNumberPositiveTest() {
+    trainStationTest.setNewTrainNumber(13,40);
+    assertEquals(40, trainStationTest.searchByTrainNumber(40).next().getTrainNumber(),
+        "The train number was not changed");
+  }
+}
